58062e6c02bbef836b9af8cdc52bc8a7ab74f47d
MinQ <minq.an7@gmail.com>
2024-11-26 15:03:12 +0900

    작업 끝 : UI확인용 세팅 상태

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // * 배포전 main.js > style.css 추가
 // * 이미지 주소 변경 > ./public/images > ./images
 // * style.css에 있는 background Image 주소 또한 ./public/images > ../images (dist 구조 때문에)
-// * 폰트 주소 변경 필요 >
+// * style.css에 있는 폰트 주소 변경 필요 > ./public/fonts > ./fonts
 // * 모두 세팅이 마치면 yarn deploy 실행 > 배포
 
 // * [상담하기, 사용문의하기], [데모 체험하기] 버튼 클릭 시 스크롤 이동
